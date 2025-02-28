ef284310e0497db07603c44744203da5f7ad2e75
NIMA <nima85sohrabi@gmail.com>
2025-02-28 16:27:06 +0330

    better of isloading

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import Actor from "../components/actor/actor/Actor";
-import NavData from "../components/header/NavData";
+// import NavData from "../components/header/NavData";
 import HomeMainGrid from "../components/HomeMainGrid";
 import PostProvider from "../components/post/post/PostProvider";
 import TrendingSwiper from "../components/post/post/TrendingSwiper";
@@ -8,7 +8,7 @@ import Type from "../components/Type";
 const HomePage = () => {
   return (
     <>
-      <NavData />
+      {/* <NavData /> */}
       <section className="w-[98%] md:w-[80%] mx-auto">
         <PostProvider>
           <main className="sm:h-[600px] my-8 flex flex-col items-center ">
